abf077b82b75340a92a89ef7573277fbdda9c8ec
protonnote <nongnumainaka@gmail.com>
2021-01-12 13:59:48 +0700

    send

--- app_template.js
+++ app_template.js
@@ -1,1 +1,25 @@
 //your code here!
+
+document.getElementById("button-compute").addEventListener("click",function(event){
+    event.preventDefault()
+    let num = document.getElementById('input-number')
+    console.log(num.value)
+
+    const lii = document.querySelector('#show-wow')
+
+    const lis = document.createElement()
+
+    const list_n = document.createElement('li')
+    const hh = document.createElement('h2')
+    hh.innerHTML = num.value
+    list_n.append(hh)
+    lii.append(list_n)
+
+},false)
+
+
+
+document.getElementById("button-create-table").addEventListener("click",function(event){
+    event.preventDefault()
+
+},false)
